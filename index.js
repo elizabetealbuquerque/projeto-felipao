392dd6ca44f26d125c31f4d2f95d7fb8c81d6be8
elizabetealbuquerque <elizabete_barbosa2@hotmail.com>
2024-10-16 14:33:51 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let nomeHeroi = "Elizabete";
+let xpHeroi = 8500;
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else if (xpHeroi > 10000) {
+    nivel = "Radiante";
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível ${nivel}.`);
